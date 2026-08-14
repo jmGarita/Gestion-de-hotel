43b0e86aa5525d54fd6ffa263b93c785fbf1d975
Montoya <jordana.montoya.garita@est.una.ac.cr>
2026-08-13 22:38:01 -0600

    se hace huespedes y su respectivo panel

---
+++ src/GestionHotelera/Huespedes.java
@@ -0,0 +1,80 @@
+package GestionHotelera;
+
+public class Huespedes {
+
+    private int id;
+    private  String nombre;
+    private String apellido;
+    private String identificacion;
+    private String telefono;
+    private String correo;
+    private String nacionalidad;
+
+    public Huespedes(int id, String nombre,String apellido, String identificacion, String telefono, String correo, String nacionalidad){
+
+        this.id=id;
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.identificacion=identificacion;
+        this.telefono=telefono;
+        this.correo=correo;
+        this.nacionalidad=nacionalidad;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
